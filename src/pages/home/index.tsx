8cb0a77d4d6932d651cebec5884b0cb2cbe9253a
José Lucas <josluc2016@gmail.com>
2023-12-31 03:27:42 -0300

    feat: left menu

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,67 +4,83 @@ import ProductComponent from "../../components/product";
 
 export default function Home(){
     const products = [
+
         {
             name: 'Café',
             price: 20.00,
-            img: 'shopping-cart64.png'
+            img: 'soft-drink.png'
         },
         {
             name: 'Café',
             price: 20.00,
-            img: 'shopping-cart64.png'
+            img: 'soft-drink.png'
         },
         {
             name: 'Café',
             price: 20.00,
-            img: 'shopping-cart64.png'
+            img: 'soft-drink.png'
         },
         {
             name: 'Café',
             price: 20.00,
-            img: 'shopping-cart64.png'
+            img: 'soft-drink.png'
         },
         {
             name: 'Café',
             price: 20.00,
-            img: 'shopping-cart64.png'
+            img: 'soft-drink.png'
         },
         {
             name: 'Café',
             price: 20.00,
-            img: 'shopping-cart64.png'
+            img: 'soft-drink.png'
         },
         {
             name: 'Café',
             price: 20.00,
-            img: 'shopping-cart64.png'
+            img: 'soft-drink.png'
         },
         {
             name: 'Café',
             price: 20.00,
-            img: 'shopping-cart64.png'
+            img: 'soft-drink.png'
         },
         {
             name: 'Café',
             price: 20.00,
-            img: 'shopping-cart64.png'
+            img: 'soft-drink.png'
         },
         {
             name: 'Café',
             price: 20.00,
-            img: 'shopping-cart64.png'
-        },
-        {
-            name: 'Café',
-            price: 20.00,
-            img: 'shopping-cart64.png'
+            img: 'soft-drink.png'
         },
     ]
+
+    const categories = ['Drinks', 'Seafood', 'Vegan']
+
+    const leftMenu = document.getElementById('left-menu')
+    const coverPage = document.getElementById('cover-page')
+
+    const handleActivateLeftMenu = ()=>{
+        if(leftMenu && coverPage){
+            leftMenu.style.display = 'flex'
+            coverPage.style.display = 'flex'
+        }
+        
+    }
+    const handleDeactivateLeftMenu = ()=>{
+        if(leftMenu && coverPage){
+            leftMenu.style.display = 'none'
+            coverPage.style.display = 'none'
+        }
+    }
+
     return(
         <div className="main-home">
             <div className="top-bar">
                 <div className="menu-name">
-                    <img src={process.env.PUBLIC_URL + 'images/menu64.png'} alt="" />
+                    <img src={process.env.PUBLIC_URL + 'images/menu64.png'} alt="" onClick={()=>handleActivateLeftMenu()} />
                     <p>Restaurant Name</p>
                 </div>
                 
@@ -82,10 +98,19 @@ export default function Home(){
                     }
                 </div>
             </div>
-            <div className="page-options">
-                    <img src={process.env.PUBLIC_URL + 'images/left-arrow.png'} alt="" />
-                    <img src={process.env.PUBLIC_URL + 'images/right-arrow.png'} alt="" />
+
+            <div className="left-menu" id="left-menu">
+                <img src={process.env.PUBLIC_URL + 'images/close.png'} alt="" className="close-left-menu" onClick={()=> handleDeactivateLeftMenu()}/>
+                <img src={process.env.PUBLIC_URL + 'images/logo.webp'} alt="" className="logo" />
+                <div className="menu-category-options">
+                    {
+                        categories.map((e)=>(
+                            <p>{e}</p>
+                        ))
+                    }
                 </div>
+            </div>
+            <div className="cover-page" id="cover-page" onClick={()=>handleDeactivateLeftMenu()}></div>
         </div>
     )
 }
