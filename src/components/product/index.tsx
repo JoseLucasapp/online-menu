5f54440dc9f21208eacdba5e158c2ec62135a761
José Lucas <josluc2016@gmail.com>
2024-01-12 14:54:44 -0300

    feat: center menu

--- src/components/product/index.tsx
+++ src/components/product/index.tsx
@@ -5,13 +5,13 @@ export interface ProductInterface{
     name: string;
     price: number;
     img: string;
+    handleActivateCenterMenu:()=> void;
 }
 
-export default function ProductComponent({img, price, name}: ProductInterface){
-
+export default function ProductComponent({img, price, name, handleActivateCenterMenu}: ProductInterface){
 
     return(
-        <div className="product-main">
+        <div className="product-main" onClick={()=> handleActivateCenterMenu()}>
             <img src={process.env.PUBLIC_URL + 'images/' + img} alt="" />
            <div className="text">
            <p>{name}</p>
