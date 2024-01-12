edcc7079e311d89658921e4af90413e1c47a0be2
José Lucas <josluc2016@gmail.com>
2024-01-12 15:47:48 -0300

    feat: api mock

---
+++ src/api.ts
@@ -0,0 +1,22 @@
+export const API_DATA =[
+    {
+        category: 'drinks',
+        products: [
+            {
+                name: 'Cocktail',
+                img: `${process.env.PUBLIC_URL}/images/soft-drink`,
+                price: 17.75
+            }
+        ]
+    },
+    {
+        category: 'Vegan',
+        products: [
+            {
+                name: 'Natural Sandwich',
+                img: `${process.env.PUBLIC_URL}/images/soft-drink`,
+                price: 7.10
+            }
+        ]
+    }
+]
